ef8d43379bb75771d925ab227caa68a194c6b7df
Tester <seumworldinbangladesh@gmail.com>
2021-04-07 00:50:54 +0600

    ctp

--- functions.php
+++ functions.php
@@ -7,7 +7,33 @@
  * @package CO_1
  */
 
-
+// Our custom post type function
+function create_posttype() {
+ 
+    register_post_type( 'movies',
+    // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Movies' ),
+                'singular_name' => __( 'Movie' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'movies'),
+            'show_in_rest' => true,
+ 
+        )
+    );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+ 
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'movies' ) );
+    return $query;
+}
 function register_my_menus() {
 	register_nav_menus(
 	  array(
