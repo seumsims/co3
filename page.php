b3a471133b77b3ad6790a03c5b8b24f76ed2a3b7
Tester <seumworldinbangladesh@gmail.com>
2021-04-07 00:40:00 +0600

    test3 done and closing now

--- page.php
+++ page.php
@@ -1,38 +1,49 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package CO_1
- */
 
 get_header();
 ?>
+<?php var_dump(the_content() . the_content()); ?>
+<main id="primary" class="site-main">
 
-	<main id="primary" class="site-main">
+<?php echo "page";
+//require get_template_directory() . '/template-parts/test.php' ;
 
-		<?php 
-		while ( have_posts() ) :
-			the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+var_dump(get_template_directory());
+if ( have_posts() ) :
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
+	if ( is_home() && ! is_front_page() ) :
 		?>
+		<header>
+			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+		</header>
+		<?php
+	endif;
 
-	</main><!-- #main -->
+	/* Start the Loop */
+	while ( have_posts() ) :
+		the_post();
+
+		/*
+		 * Include the Post-Type-specific template for the content.
+		 * If you want to override this in a child theme, then include a file
+		 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+		 */
+		require get_template_directory() . '/template-parts/test.php' ;
+
+	endwhile;
+
+	the_posts_navigation();
+
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+
+endif;
+?>
+
+</main><!-- #main -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
